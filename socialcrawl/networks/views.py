72173e468754918d242f944f6db5e724059f7d35
Miquel Torres <tobami@gmail.com>
2013-08-24 18:52:12 +0200

    Use CachedClients to dynamically fetch data

--- socialcrawl/networks/views.py
+++ socialcrawl/networks/views.py
@@ -1,8 +1,13 @@
+from socialcrawl.clients.crawler import CachedTwitterClient, CachedFacebookClient
 from socialcrawl.networks.models import Profile
 from socialcrawl.networks.api import APIResponse
 
 SUPPORTED_NETWORKS = [n[1].lower() for n in Profile.SOCIAL_NETWORKS]
 
+CLIENTS = {
+    'twitter': CachedTwitterClient(),
+    'facebook': CachedFacebookClient(),
+}
 
 def notfound(request):
     return APIResponse({'error': 'Not Found'}, status=404)
@@ -15,15 +20,15 @@ def networks(request):
 
 def profiles(request, network, username=None):
     if network not in SUPPORTED_NETWORKS:
-        APIResponse({'error': 'Network Not Supported'}, status=404)
+        return APIResponse({'error': 'Network Not Supported'}, status=404)
     else:
-        network = network[0].upper()
+        network_key = network[0].upper()
     if username:
         try:
-            data = Profile.objects.get(username=username, network=network)
+            data = Profile.objects.get(username=username, network=network_key)
         except:
-            raise
+            data = CLIENTS[network].get_profile(username)
         return APIResponse(data)
     else:
-        data = Profile.objects.filter(network=network)
+        data = Profile.objects.filter(network=network_key)
         return APIResponse(data)
